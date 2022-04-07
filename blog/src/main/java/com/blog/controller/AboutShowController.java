2d7ce9872b96647119b06b4b608a5cf7cbcc17a4
doudou <184264681@qq.com>
2022-04-07 14:52:56 +0800

    去掉html中多余的代码

--- blog/src/main/java/com/blog/controller/AboutShowController.java
+++ blog/src/main/java/com/blog/controller/AboutShowController.java
@@ -3,6 +3,8 @@ package com.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+/*关于我页面*/
 @Controller
 public class AboutShowController {
 
