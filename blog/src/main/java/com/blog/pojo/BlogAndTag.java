efd0f5983916d9968be00e5b06885f98c5536e78
doudou <184264681@qq.com>
2022-04-07 16:04:43 +0800

    根据别人的修改

--- blog/src/main/java/com/blog/pojo/BlogAndTag.java
+++ blog/src/main/java/com/blog/pojo/BlogAndTag.java
@@ -8,14 +8,15 @@ import lombok.NoArgsConstructor;
  * 把博客和标签关系存到数据库中使用的类
  */
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class BlogAndTag {
 
     private Long tagId;
 
     private Long blogId;
+
+    public BlogAndTag(Long id, Long id1) {
+    }
 
     public Long getTagId() {
         return tagId;
